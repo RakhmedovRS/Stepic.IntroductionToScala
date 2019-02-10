6fe62e20ac2b0649e92dfc5f5325ee64e16e5982
rassoll <rassoll32@gmail.com>
2019-02-10 18:36:18 +0300

    Добавлено решение 1-й задачи

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "Stepic.IntroductionToScala"
+
+version := "1.0"
+
+scalaVersion := "2.12.8"
+    
---
+++ src/main/scala/Chapter2_1/step3/Main.scala
@@ -0,0 +1,7 @@
+package Chapter2_1.step3
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hi, " + readLine() + "!")
+  }
+}
